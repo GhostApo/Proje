da3d8d3f92060693d5b9ac402efd26e92c97ae7e
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 18:15:08 +0300

    Add files via upload

---
+++ Random/Random/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int baslangıc = Convert.ToInt32(textBox1.Text);
+            int bitis = Convert.ToInt32(textBox2.Text);
+            Random random = new Random();
+            int sonuc = random.Text(baslangıc, bitis+1);
+            textBox3.Text = sonuc.ToString();
+        }
+    }
+}
