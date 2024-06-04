4e255c8e8d81fe707b8b41731d788d82217e66d4
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 18:22:05 +0300

    Add files via upload

---
+++ VarsayılanParametreliMethod/VarsayılanParametreliMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarsayılanParametreliMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sinif s = new Sinif();
+            s.Selamla();
+            s.Selamla("Deniz");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VarsayılanParametreliMethod/VarsayılanParametreliMethod/Sinif.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarsayılanParametreliMethod
+{
+    class Sinif
+    {
+        public void Selamla(string ad = "Alper")
+        {
+            Console.WriteLine("Merhaba " + ad);
+        }
+    }
+}
