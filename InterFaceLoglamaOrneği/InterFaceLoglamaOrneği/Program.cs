4fd68e28a382f1d66720e144f31c0e4e71b40037
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 18:20:00 +0300

    Add files via upload

---
+++ InterFaceLoglamaOrneği/InterFaceLoglamaOrneği/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaceLoglamaOrneği
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new DatabaseLogger();
+            customerManager.Ekle();
+            Console.ReadLine();
+        }
+    }
+}
