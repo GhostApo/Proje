a62009645c622e80216ad7375d77039a0001f91e
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 18:20:56 +0300

    Add files via upload

---
+++ InterfaceUygulama/InterfaceUygulama/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceUygulama
+{
+    class Manager:IWorker,ISalary,IEat
+    {
+        public void Work()
+        {
+            Console.WriteLine("Yönetici çalışır");
+        }
+
+        public void getSalary()
+        {
+            Console.WriteLine("Yönetici maaş alır");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Yemek yer");
+        }
+    }
+}
---
+++ InterfaceUygulama/InterfaceUygulama/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceUygulama
+{
+    class Worker:IWorker,ISalary,IEat
+    {
+        public void Work()
+        {
+            Console.WriteLine("Worker çalışıyor");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Yemek yer");
+        }
+
+        public void getSalary()
+        {
+            Console.WriteLine("Worker maaş alır");
+        }
+    }
+}
