55e2ffc10ab67206e6d0380c69531b8c23c241f9
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 17:40:56 +0300

    Add files via upload

---
+++ Uygulama/Uygulama/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int baslangic = Convert.ToInt32(textBox1.Text);
+            int bitis = Convert.ToInt32(textBox2.Text);
+            for (int i = baslangic; i <= bitis; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    listBox1.Items.Add(i.ToString());
+                }
+            }
+        }
+    }
+}
