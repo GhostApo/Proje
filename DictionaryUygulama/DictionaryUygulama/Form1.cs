fb85741b7bae9206d5f3f64bf376f5c217761faf
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 17:34:57 +0300

    Add files via upload

---
+++ DictionaryUygulama/DictionaryUygulama/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DictionaryUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int, string> ogrenciler = new Dictionary<int, string>();
+        int anahtar;
+        string deger;
+
+        private void Listele()
+        {
+            listBox1.Items.Clear();
+            foreach (var ogrenci in ogrenciler)
+            {
+                listBox1.Items.Add(ogrenci.Key + " - " + ogrenci.Value);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool durum = false;
+            // okul no yada ad soyad olup olomadığı kontrol edilir.
+            if (textBox1.Text!=" ") // textBox1 boş değilse
+            {
+                anahtar = Convert.ToInt32(textBox1.Text); // okul no
+                durum = ogrenciler.ContainsKey(anahtar);
+            }
+            else
+            {
+                anahtar = Convert.ToInt32(textBox1.Text); // okul no
+                durum = ogrenciler.ContainsValue(deger);
+            }
+
+            if (durum == true)
+            {
+                MessageBox.Show("Öğrenci kayıtlıdır");
+            }
+            else
+            {
+                MessageBox.Show("Öğrenci kayıtlı değil");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            anahtar = Convert.ToInt32(textBox1.Text); // OKul No
+            deger = textBox2.Text; // Ad Soyad
+            ogrenciler.Add(anahtar, deger); // OKul No - Ad Soyad
+            Listele();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            anahtar = Convert.ToInt32(textBox1.Text); // OKul No
+            deger = textBox2.Text; // Ad Soyad
+            ogrenciler[anahtar] = deger;
+            Listele();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            anahtar = Convert.ToInt32(textBox1.Text);
+            ogrenciler.Remove(anahtar);
+            // anahtarlar silinirse tuttuğu değer de silinir
+            Listele();
+        }
+    }
+}
---
+++ HashtableKoleksiyonu/HashtableKoleksiyonu/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtableKoleksiyonu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hastable nesnesi oluşturma
+            Hashtable ogrenciler = new Hashtable();
+            ogrenciler.Add(550, "Alper"); // veri ekleme 1.yol
+            ogrenciler.Add("Alper", 1572); // veri ekleme 2.yol
+            ogrenciler.Add(551, "Emirhan");
+            ogrenciler.Add(552, "Deniz");
+            ogrenciler.Add(553, "Salih");
+            ogrenciler.Add(554, "Süleyman Çakır (Berkay)");
+
+            // silme
+            ogrenciler.Remove(550);
+            ogrenciler.Remove("Çikolatalı Ekmek");
+
+            // güncelleme
+            ogrenciler[554] = "Midi";
+
+            foreach (var anahtar in ogrenciler.Keys)
+            {
+                Console.WriteLine(anahtar);
+            }
+            foreach (var deger in ogrenciler.Keys)
+            {
+                Console.WriteLine(deger);
+            }
+
+            foreach (DictionaryEntry eleman in ogrenciler)
+            {
+                Console.WriteLine(eleman.Key + " - " + eleman.Value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
