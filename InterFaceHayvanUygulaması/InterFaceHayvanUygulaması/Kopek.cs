f6192703dc0a17c1225ba71325361dc375fddd0d
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 18:19:32 +0300

    Add files via upload

---
+++ InterFaceHayvanUygulaması/InterFaceHayvanUygulaması/Kopek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterFaceHayvanUygulaması
+{
+    class Kopek : IHayvan , IBeslen
+    {
+        public void Beslen()
+        {
+            Console.WriteLine("Köpek Et Yedi");
+        }
+
+        public void SesCikar()
+        {
+            Console.WriteLine("Köpek : hav hav");
+        }
+    }
+}
