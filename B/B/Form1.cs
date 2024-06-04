1fecb4e00107b89c602f9efefc0c57702088ee3c
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 18:06:50 +0300

    Add files via upload

---
+++ B/B/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int not;
+            not = Convert.ToByte(textBox1.Text);
+            if (not >= 51)
+            {
+                MessageBox.Show("Sınıfı geçtiniz.");
+            }
+            if (not >= 85)
+            {
+                MessageBox.Show("Taktir aldınız.");
+            }
+            
+            else if (not >= 70 & not < 85)
+            {
+                MessageBox.Show("Teşekkür aldınız.");
+            }
+            else if (not >= 0 & < 49)
+            {
+                MessageBox.Show("Sınıfta kaldınız.");
+            }
+            else if (not >= 100)
+            {
+                MessageBox.Show("Yanlış değer girdiniz.");
+            }
+            if (not == 50)
+            {
+                MessageBox.Show("Geçtiniz.");
+            }
+            else if (not >= 50 & not < 70)
+            {
+                MessageBox.Show("Belge almadan geçtiniz.");
+            }
+           
+        }
+    }
+}
