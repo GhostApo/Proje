5336e5397b80787fe41f7a2949c550024ee8c692
Sade Alper :D <156118537+GhostApo@users.noreply.github.com>
2024-06-04 18:17:10 +0300

    Add files via upload

---
+++ SortedListKoleksiyonu/SortedListKoleksiyonu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedListKoleksiyonu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SortedList artan şekilde sıralanır
+            // sortedList koleksiyonunun nesnesini oluşturma
+            SortedList ogrenciler = new SortedList();
+            ogrenciler.Add(368, "Ahmet"); // veri ekleme
+            ogrenciler.Remove(388); // veri silme
+            ogrenciler[368] = "Mehmet"; // güncelleme
+            foreach (DictionaryEntry eleman in ogrenciler)
+            {
+                Console.WriteLine(eleman.Key+ " - " + eleman.Value);
+            }
+        }
+    }
+}
